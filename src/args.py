475a6043127a2a03bdeb31338112c5f63657719b
Tanya <tanyach1997@gmail.com>
2022-05-16 22:18:19 +0530

    initial commit

--- src/args.py
+++ src/args.py
@@ -5,7 +5,9 @@ def get_args_parser():
     parser = argparse.ArgumentParser('Set transformer detector', add_help=False)
     parser.add_argument('--lr', default=1e-4, type=float)
     parser.add_argument('--lr_backbone', default=1e-5, type=float)
-    parser.add_argument('--batch_size', default=2, type=int)
+    parser.add_argument('--batch_size', default=2, type=int)    
+    parser.add_argument('--grad_accum_batches', default=1, type=int,
+                        help="Number of batches to accumulate using gradient accumulation")
     parser.add_argument('--weight_decay', default=1e-4, type=float)
     parser.add_argument('--epochs', default=300, type=int)
     parser.add_argument('--lr_drop', default=200, type=int)
